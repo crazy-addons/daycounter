103ed727fd2adfc522030cd6e4a40c90a9252483
JanK <jan.web@outlook.de>
2024-11-05 12:44:52 +0100

    Playing Sound

--- core/src/main/java/net/crazy/daycounter/core/DayAnnouncer.java
+++ core/src/main/java/net/crazy/daycounter/core/DayAnnouncer.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import net.labymod.api.client.chat.ChatExecutor;
 import net.labymod.api.client.component.Component;
+import net.labymod.api.client.resources.ResourceLocation;
 import net.labymod.api.notification.Notification;
 import net.labymod.api.util.I18n;
 import net.labymod.api.util.concurrent.task.Task;
@@ -16,6 +17,7 @@ public class DayAnnouncer {
 
     private final DayCounter addon;
     private final ChatExecutor chatExecutor;
+    private final ResourceLocation soundLocation;
 
     private final Component prefix = Component.text("-- "),
         suffix = Component.text(" --");
@@ -23,6 +25,11 @@ public class DayAnnouncer {
     public DayAnnouncer(DayCounter addon) {
         this.addon = addon;
         this.chatExecutor = addon.labyAPI().minecraft().chatExecutor();
+
+        String version = addon.labyAPI().minecraft().getVersion();
+        String path = version.equals("1.8.9") ? "note.pling" :
+            version.equals("1.12.2") ? "block.note.pling" : "block.note_block.pling";
+        this.soundLocation = ResourceLocation.create("minecraft", path);
     }
 
     public void announce() {
@@ -40,6 +47,7 @@ public class DayAnnouncer {
                 .append(Component.translatable("daycounter.messages.day"))
                 .append(Component.text(" " + getDay()))
                 .append(suffix));
+        playSound(1f);
     }
 
 
@@ -57,15 +65,19 @@ public class DayAnnouncer {
                 return;
             }
 
+            boolean inProgress = counter.get() < announcement.length() - 1;
+
             chatExecutor.displayActionBar(
                 Component.empty()
                     .append(prefix)
                     .append(Component.text(
-                        counter.get() < announcement.length() ?
-                            announcement.substring(0, counter.get() + 1) : announcement
+                        inProgress ? announcement.substring(0, counter.get() + 1) : announcement
                     ))
                     .append(suffix)
             );
+
+            playSound(inProgress ? .5f : 1f);
+
             counter.incrementAndGet();
         }).repeat(500, TimeUnit.MILLISECONDS).build();
         task.executeOnRenderThread();
@@ -75,10 +87,18 @@ public class DayAnnouncer {
         Notification.builder()
             .title(Component.translatable("daycounter.messages.day")
                 .append(Component.text(" " + getDay())));
+        playSound(1f);
     }
 
     private int getDay() {
         return (int) (addon.labyAPI().minecraft().clientWorld().getDayTime() / 24000) +
             (addon.configuration().startAtOne.getOrDefault() ? 1 : 0);
+    }
+
+    private void playSound(float pitch) {
+        if (!addon.configuration().withSound.getOrDefault())
+            return;
+
+        addon.labyAPI().minecraft().sounds().playSound(soundLocation, .3f, pitch);
     }
 }
