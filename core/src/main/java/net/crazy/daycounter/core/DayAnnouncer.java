c1abb67707bd99984ea7f75a891196ea646be3df
JanK <jan.web@outlook.de>
2024-11-05 12:12:22 +0100

    Added Actionbar Type animation

--- core/src/main/java/net/crazy/daycounter/core/DayAnnouncer.java
+++ core/src/main/java/net/crazy/daycounter/core/DayAnnouncer.java
@@ -1,15 +1,24 @@
 package net.crazy.daycounter.core;
 
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import net.labymod.api.client.chat.ChatExecutor;
 import net.labymod.api.client.component.Component;
+import net.labymod.api.notification.Notification;
+import net.labymod.api.util.I18n;
+import net.labymod.api.util.concurrent.task.Task;
 
 /**
  * @author cs-jako
  */
 public class DayAnnouncer {
+
     private final DayCounter addon;
     private final ChatExecutor chatExecutor;
+
+    private final Component prefix = Component.text("-- "),
+        suffix = Component.text(" --");
 
     public DayAnnouncer(DayCounter addon) {
         this.addon = addon;
@@ -18,7 +27,7 @@ public class DayAnnouncer {
 
     public void announce() {
         switch (addon.configuration().channel.getOrDefault()) {
-            case ACTIONBAR_QUICK -> announceActionBarQuick();
+            case ACTIONBAR -> announceActionBarQuick();
             case NOTIFICATION -> announceNotification();
             case null, default -> announceActionBarTyped();
         }
@@ -26,20 +35,50 @@ public class DayAnnouncer {
 
     private void announceActionBarQuick() {
         chatExecutor.displayActionBar(
-            Component.text("-- ")
+            Component.empty()
+                .append(prefix)
                 .append(Component.translatable("daycounter.messages.day"))
-                .append(Component.text(" " + getDay() + " --")));
+                .append(Component.text(" " + getDay()))
+                .append(suffix));
     }
 
-    private void announceActionBarTyped() {
 
+    private Task task;
+
+    private void announceActionBarTyped() {
+        AtomicInteger counter = new AtomicInteger(0);
+        int day = getDay();
+        String message = I18n.translate("daycounter.messages.day");
+        String announcement = message + " " + day;
+
+        task = Task.builder(() -> {
+            if (counter.get() >= announcement.length()) {
+                task.cancel();
+                return;
+            }
+
+            chatExecutor.displayActionBar(
+                Component.empty()
+                    .append(prefix)
+                    .append(Component.text(
+                        counter.get() < announcement.length() ?
+                            announcement.substring(0, counter.get() + 1) : announcement
+                    ))
+                    .append(suffix)
+            );
+            counter.incrementAndGet();
+        }).repeat(500, TimeUnit.MILLISECONDS).build();
+        task.executeOnRenderThread();
     }
 
     private void announceNotification() {
-
+        Notification.builder()
+            .title(Component.translatable("daycounter.messages.day")
+                .append(Component.text(" " + getDay())));
     }
 
     private int getDay() {
-        return (int) (addon.labyAPI().minecraft().clientWorld().getDayTime() / 24000);
+        return (int) (addon.labyAPI().minecraft().clientWorld().getDayTime() / 24000) +
+            (addon.configuration().startAtOne.getOrDefault() ? 1 : 0);
     }
 }
