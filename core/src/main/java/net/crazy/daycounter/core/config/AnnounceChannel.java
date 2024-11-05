2146ec083f28273c6748bd5338367637701e0a92
JanK <jan.web@outlook.de>
2024-11-05 12:11:12 +0100

    Fixed i18n

--- core/src/main/java/net/crazy/daycounter/core/config/AnnounceChannel.java
+++ core/src/main/java/net/crazy/daycounter/core/config/AnnounceChannel.java
@@ -5,7 +5,7 @@ package net.crazy.daycounter.core.config;
  * @author cs-jako
  */
 public enum AnnounceChannel {
-    ACTIONBAR_QUICK,
-    ACTIONBAR_TYPED,
+    ACTIONBAR, // ACTIONBAR_QUICK
+    TYPED, // ACTIONBAR_TYPED
     NOTIFICATION;
 }
